210f9ffdd9820d910b7b3b78b9e135279e4e30d6
João Borges <joaopauloborges1995@gmail.com>
2023-02-25 11:28:25 -0300

    add game chess

---
+++ xadrez/src/config.py
@@ -0,0 +1,49 @@
+import pygame
+
+win = pygame.display.set_mode ((800, 800))
+
+selected_check = False 
+is_pawn_promotion = False 
+white_moving = True 
+wcastle = [True, True]
+bcastle = [True, True]
+holding = False
+checkmated = False
+restart_game = False
+rotated = False
+menu = True
+play_move = False
+multiplayer = True
+
+menu_img = pygame.image.load("../assets/menu.png")
+board_img = pygame.image.load("../assets/board.png")
+target = pygame.image.load("../assets/target.png")
+cross = pygame.image.load("../assets/cross.png")
+rotate = pygame.image.load("../assets/rotate.png")
+arrow_backwards = pygame.image.load("../assets/arrow_backwards.png")
+arrow_forwards = pygame.image.load("../assets/arrow_forwards.png")
+return_menu = pygame.image.load("../assets/return_menu.png")
+(wpawn, bpawn) = (pygame.image.load("../assets/wpawn.png"), pygame.image.load("../assets/bpawn.png"))
+(wknight, bknight) = (pygame.image.load("../assets/wknight.png"), pygame.image.load("../assets/bknight.png"))
+(wbishop, bbishop) = (pygame.image.load("../assets/wbishop.png"), pygame.image.load("../assets/bbishop.png"))
+(wrook, brook) = (pygame.image.load("../assets/wrook.png"), pygame.image.load("../assets/brook.png"))
+(wqueen, bqueen) = (pygame.image.load("../assets/wqueen.png"), pygame.image.load("../assets/bqueen.png"))
+(wking, bking) = (pygame.image.load("../assets/wking.png"), pygame.image.load("../assets/bking.png"))
+
+board = [[brook, bknight, bbishop, bqueen, bking, bbishop, bknight, brook],
+        [bpawn, bpawn, bpawn, bpawn, bpawn, bpawn, bpawn, bpawn],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [wpawn, wpawn, wpawn, wpawn, wpawn, wpawn, wpawn, wpawn],
+        [wrook, wknight, wbishop, wqueen, wking, wbishop, wknight, wrook]]
+
+new_board = [[0, 0, 0, 0, 0, 0, 0, 0] for _ in range(8)]
+previou_board = [[0, 0, 0, 0, 0, 0, 0, 0] for _ in range(8)]
+board_history = {}
+prev_board = [[0, 0, 0, 0, 0, 0, 0, 0] for _ in range(8)]
+for (x,y) in [(x,y) for x in range(8) for y in range(8)]:
+    prev_board[y][x] = board[y][x]
+move = 0
+board_history[move] = prev_board
